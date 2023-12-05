83bdefefe49fa45e2df276df3404fa6ea16a9c81
atabekis <a.bekishoglu@student.tue.nl>
2023-12-05 16:50:04 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Copyright (C) 2023-2024 Eindhoven University of Technology & Tilburg University
+JBC090 - Language & AI
+
+Aadersh Kalyanasundaram, Ata Bekişoğlu, Egehan Kabacaoğlu, Emre Kamal
+"""
+
+if __name__ == '__main__':
+    pass
