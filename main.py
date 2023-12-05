0c78fea5c765992e6d697b4370a2a283b28268cf
atabekis <a.bekishoglu@student.tue.nl>
2023-12-05 17:57:37 +0100

    exploring the dataset in explore.ipynb

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Copyright (C) 2023-2024 Eindhoven University of Technology & Tilburg University
 JBC090 - Language & AI
 
 Aadersh Kalyanasundaram, Ata Bekişoğlu, Egehan Kabacaoğlu, Emre Kamal
+
+If the code doesn't run when cloning from GitHub, the data folder is assumed to be missing.
 """
 
 if __name__ == '__main__':
